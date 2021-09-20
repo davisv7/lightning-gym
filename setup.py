0605bf09f2f6d2f79189b7dfa32ee40249481dae
davisv7 <vincentmdavis@protonmail.com>
2021-09-20 13:46:20 -0400

    packages

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Gym Environment for the Bitcoin Lightning Network',
     author='Vincent',
     author_email='vmda225@uky.edu',
-    packages=find_packages(include=['lightning_gym']),  # same as name
+    packages=find_packages(include=['lightning_gym', 'lightning_gym.*']),  # same as name
     install_requires=[
         "gym",
         "numpy",
