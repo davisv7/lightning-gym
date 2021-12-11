6111d8120846075a994097ba43a1c265d9d5e1d1
v1ntage <vincentmdavis@protonmail.com>
2021-12-10 19:28:12 -0500

    only downsample if the graph type is snapshot

--- main.py
+++ main.py
@@ -28,37 +28,43 @@ def main():
     if seed:
         print("seed set")
         random_seed(seed)
-    nodes, edges = load_json(path.join(getcwd(), json_filename))
 
-    # clean nodes
-    active_nodes = get_pubkeys(nodes)
+    if config["env"]["graph_type"] == "snapshot":
+        nodes, edges = load_json(path.join(getcwd(), json_filename))
 
-    # clean edges
-    edge_filters = config["edge_filters"]
-    active_edges = clean_edges(edges, edge_filters)
-    active_edges = get_channels_with_attrs(active_edges)
+        # clean nodes
+        active_nodes = get_pubkeys(nodes)
 
-    # Create graph
-    g = nx.MultiDiGraph()
-    g.add_edges_from(active_edges)
-    g = nx.MultiDiGraph(g.subgraph(active_nodes))
-    if ds:
-        g = down_sample(g, config)
-    # clean graph
-    graph_filters = config["graph_filters"]
-    if graph_filters.getboolean("combine_multiedges"):
-        g = simplify_graph(g)
-    if graph_filters.getboolean("remove_bridges"):
-        g = nx.DiGraph(reduce_to_mainnet(g))
-    if graph_filters.getboolean("undirected"):
-        g = undirected(g)
-    if graph_filters.getboolean("unweighted"):
-        nx.set_edge_attributes(g, values=0.1, name='cost')
+        # clean edges
+        edge_filters = config["edge_filters"]
+        active_edges = clean_edges(edges, edge_filters)
+        active_edges = get_channels_with_attrs(active_edges)
 
-    print(len(g.nodes()), len(g.edges()))
+        # Create graph
+        g = nx.MultiDiGraph()
+        g.add_edges_from(active_edges)
+        g = nx.MultiDiGraph(g.subgraph(active_nodes))
+        if ds:
+            g = down_sample(g, config)
 
-    # create an environment, an agent, and then train for some number of episodes
-    env = NetworkEnvironment(config, g=g)
+        # reduce graph
+        graph_filters = config["graph_filters"]
+        if graph_filters.getboolean("combine_multiedges"):
+            g = simplify_graph(g)
+        if graph_filters.getboolean("remove_bridges"):
+            g = nx.DiGraph(reduce_to_mainnet(g))
+        if graph_filters.getboolean("undirected"):
+            g = undirected(g)
+        if graph_filters.getboolean("unweighted"):
+            nx.set_edge_attributes(g, values=0.1, name='cost')
+
+        print(len(g.nodes()), len(g.edges()))
+
+        # create an environment, an agent, and then train for some number of episodes
+        env = NetworkEnvironment(config, g=g)
+    else:
+        env = NetworkEnvironment(config)
+
     # env = NetworkEnvironment(config)
     ajay = DiscreteActorCritic(env, config)
     rando = RandomAgent(env)
@@ -69,7 +75,7 @@ def main():
         log = ajay.train()
         recommendations = env.get_recommendations()
         print("E: {}, R: {:.4f}, N:{}".format(episode, log.log['tot_reward'][-1], recommendations))
-        # ajay.save_model()
+    ajay.save_model()
 
     ajay._test = True
     print("Test Results:", ajay.test())
