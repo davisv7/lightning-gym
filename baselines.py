97c94d879642db981e367e710a9ace98e76539b5
v1ntage <vincentmdavis@protonmail.com>
2021-12-13 20:12:10 -0500

    added TrainedGreedyAgent no calc flag

--- baselines.py
+++ baselines.py
@@ -1,5 +1,8 @@
 import numpy as np
 from lightning_gym.envs.lightning_network import NetworkEnvironment
+import torch
+import torch.nn.functional as F
+from lightning_gym.GCN import GCN
 
 
 class TopKAgent:
@@ -17,12 +20,12 @@ class TopKAgent:
             action = self.pick_topk_action()
 
             # take action
-            _, reward, done, _ = self.problem.step(action, test=True)  # Take action and find outputs
-
+            _, _, done, _ = self.problem.step(action, no_calc=True)  # Take action and find outputs
+        self.problem.get_reward()
         return self.problem.btwn_cent
 
     def compute_betweenness(self):
-        betweennesses = self.problem.ig_g.betweenness(directed=True, weights="cost")
+        betweennesses = self.problem.ig_g.betweenness(directed=True, weights="cost", cutoff=20)
         names = self.problem.ig_g.vs()["name"]
         actions = [self.problem.index_to_node.inverse[name] for name in names]
         self.action_to_btwn = dict(zip(actions, betweennesses))
@@ -49,8 +52,8 @@ class GreedyAgent:
             action = self.pick_greedy_action()
 
             # take action
-            _, reward, done, _ = self.problem.step(action)  # Take action and find outputs
-
+            _, _, done, _ = self.problem.step(action, no_calc=True)  # Take action and find outputs
+        self.problem.get_reward()
         return self.problem.btwn_cent
 
     def pick_greedy_action(self):
@@ -70,7 +73,7 @@ class GreedyAgent:
                 if reward > best_reward:
                     best_action = action
                     best_reward = reward
-                _, _, _, _ = self.problem.step(action)
+                _, _, _, _ = self.problem.step(action, no_calc=True)
 
         return best_action
 
@@ -88,10 +91,77 @@ class RandomAgent:
             action = self.pick_random_action()
 
             # take action
-            _, _, done, _ = self.problem.step(action.item(), test=True)  # Take action and find outputs
-
+            _, _, done, _ = self.problem.step(action.item(), no_calc=True)  # Take action and find outputs
+        self.problem.get_reward()
         return self.problem.btwn_cent
 
     def pick_random_action(self):
         legal_actions = self.problem.get_legal_actions().squeeze().detach().numpy()
         return np.random.choice(legal_actions)
+
+
+class TrainedGreedyAgent:
+    def __init__(self, problem: NetworkEnvironment, config):
+        self.problem = problem  # environment
+        self.path = config.get("agent", "model_file")
+        self.in_feats = config.getint("agent", "in_features")
+        self.hid_feats = config.getint("agent", "hid_features")
+        self.out_feats = config.getint("agent", "out_features")
+        self.layers = config.getint("agent", "layers")
+        self.model = GCN(self.in_feats, self.hid_feats, self.out_feats, n_layers=self.layers, activation=F.rrelu)
+        self.load_model()
+        self.path = config.get("agent", "model_file")
+
+    def load_model(self):
+        self.model.load_state_dict(torch.load(self.path))
+        self.model.eval()
+
+    def run_episode(self):  # similar to epochs
+        done = False
+        G = self.problem.reset()  # We get our initial state by resetting
+
+        while not done:  # While we haven't exceeded budget
+            # Get action from policy network
+            action = self.pick_greedy_action(G)
+
+            # take action
+            _, _, done, _ = self.problem.step(action, no_calc=True)  # Take action and find outputs
+        self.problem.get_reward()
+        print(self.problem.btwn_cent, self.problem.get_recommendations())
+        return self.problem.btwn_cent
+
+    def pick_greedy_action(self, G):
+        illegal_actions = self.problem.get_illegal_actions().squeeze().detach().numpy()
+        best_action = None
+        n = 5
+        [pi, _] = self.model(G)
+        if self.problem.num_actions + self.problem.budget_offset < 2:
+            best_action = self.predict_action(pi, illegal_actions, 1).item()
+        else:
+            best_reward = 0
+            for action in self.predict_action(pi, illegal_actions, n):
+                a = action.item()
+                _, reward, _, _ = self.problem.step(a)
+                reward = reward.item()
+                if reward > best_reward:
+                    best_action = a
+                    best_reward = reward
+                _, _, _, _ = self.problem.step(a, no_calc=True)
+
+        return best_action
+
+    def predict_action(self, pi, illegal_actions, n=1):
+
+        # make illegal actions impossible
+        pi = pi.squeeze()
+        pi[illegal_actions] = -float('Inf')
+
+        # Calculate distribution from policy network
+        pi = F.softmax(pi, dim=0)
+        dist = torch.distributions.categorical.Categorical(pi)
+        probs = dist.probs.detach().numpy()
+
+        if n == 1:
+            return probs.argmax()  # take the most likely action
+        else:
+            return dist.sample((n,))  # sample n actions
