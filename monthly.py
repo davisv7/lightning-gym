50c2753cfabc7299e41585fd334baa05a9e031e4
v1ntage <vincentmdavis@protonmail.com>
2022-06-13 19:39:29 -0400

    monthly.py

--- monthly.py
+++ monthly.py
@@ -17,7 +17,7 @@ def gen_monthly_data(config):
     between_results = []
     degree_results = []
     # greedy_results = []
-    # trained_results = []
+    trained_results = []
     kcenter_results = []
     times = []
 
@@ -49,7 +49,7 @@ def gen_monthly_data(config):
         topk_btwn = TopBtwnAgent(env)
         topk_degree = TopDegreeAgent(env)
         # greed = GreedyAgent(env)
-        # trained = TrainedGreedyAgent(env, config)
+        trained = TrainedGreedyAgent(env, config)
         kcenter = kCenterAgent(env)
 
         baseline_list = [agent,
@@ -57,35 +57,44 @@ def gen_monthly_data(config):
                          topk_btwn,
                          topk_degree,
                          # greed,
-                         # trained,
+                         trained,
                          kcenter]
         results_list = [agent_results,
                         random_results,
                         between_results,
                         degree_results,
                         # greedy_results ,
-                        # trained_results ,
+                        trained_results,
                         kcenter_results]
 
-        start = timer()
         round_time = []
         for baseline, r_list in zip(baseline_list, results_list):
+            start = timer()
             r_list.append(baseline.run_episode())
-            round_time.append(timer() - (start+sum(round_time)))
+            round_time.append(timer() - start)
         times.append(round_time)
 
+        print("A2C Results", agent_results[-1])
+        print("Random Results:", random_results[-1])
+        print("Betweenness Results:", between_results[-1])
+        print("Degree Results:", degree_results[-1])
+        # print("Greed Results:", greedy_results[-1])
+        print("Trained Greedy Results:", trained_results[-1])
+        print("kCenter Results:", kcenter_results[-1])
+        print(times)
+        print()
         if verbose:
             print(f"Testing month {month} complete.")
 
     # performance
     df_baselines = pd.DataFrame({
-        "Agent": agent_results,
+        "A2C": agent_results,
         "Random": random_results,
         "Betweenness": between_results,
         "Degree": degree_results,
         # "Greedy": greedy_results,
-        # "Trained Greedy": trained_results,
-        "kCenter": kcenter_results
+        "Trained Greedy": trained_results,
+        "k-Center": kcenter_results
     }, index=month_prefixes)
 
     # network size
@@ -100,27 +109,33 @@ def gen_monthly_data(config):
 
     df_runtimes = pd.DataFrame(dict(zip(
         [
-            "Agent",
+            "A2C",
             "Random",
             "Betweenness",
             "Degree",
             # "Greedy",
-            # "Trained Greedy",
-            "kCenter"
+            "Trained Greedy",
+            "k-Center"
         ], zip(*times)
     )), index=month_prefixes)
 
     # save results
     df_baselines.to_pickle("monthly_data.pkl")
     df_sizes.to_pickle("sizes_data.pkl")
-    # df_runtimes.to_pickle("runtime_data.pkl")
+    df_runtimes.to_pickle("runtime_data.pkl")
 
 
 def plot_changing_month(df=None):
     if df is None:
         df = pd.read_pickle("monthly_data.pkl")
+    # df = round(df, 4)
+    df.index = [x.capitalize() for x in df.index]
+    df = df.rename(columns={"Agent": "A2C", "kCenter": "k-Center"})
+
     df.plot()
-    plt.title("Comparison of Betweenness Improvement")
+    print(df)
+    plt.title("Performance Comparison of Algorithms")
+    plt.xticks(ticks=list(range(11)), labels=df.index)
     plt.xlabel("Month")
     plt.ylabel("Betweenness Improvement")
     plt.show()
@@ -129,25 +144,32 @@ def plot_changing_month(df=None):
 def plot_network_size(df=None):
     if df is None:
         df = pd.read_pickle("sizes_data.pkl")
+    df.index = [x.capitalize() for x in df.index]
     df[['Nodes Before', 'Nodes After']].plot.bar(rot=0)
-    plt.title("Change in Number of Nodes After Filtering")
+    plt.title("Change in Number of Nodes After Pruning")
     plt.xlabel("Month")
     plt.ylabel("Number of Nodes")
     plt.show()
 
     df[['Edges Before', 'Edges After']].plot.bar(rot=0)
-    plt.title("Change in Number of Edges After Filtering")
+    plt.title("Change in Number of Edges After Pruning")
     plt.xlabel("Month")
     plt.ylabel("Number of Edges")
     plt.show()
+    print(df)
 
 
 def plot_runtimes(df=None):
     if df is None:
         df = pd.read_pickle("runtime_data.pkl")
+    df.index = [x.capitalize() for x in df.index]
+    df = df.rename(columns={"Agent": "A2C", "kCenter": "k-Center"})
+    df = round(df, 2)
     print(df)
-    df.plot()
-    plt.title("Comparison of Runtimes")
+    df.plot(logy=True)
+    # plt.yscale('log')
+    plt.xticks(ticks=list(range(11)), labels=df.index)
+    plt.title("Runtime Comparison of Algorithms")
     plt.xlabel("Month")
     plt.ylabel("Runtime (s)")
     plt.show()
@@ -161,7 +183,7 @@ if __name__ == '__main__':
     # print_config(config)
     if seed:
         random_seed(seed)
-    gen_monthly_data(config)
+    # gen_monthly_data(config)
     plot_changing_month()
     plot_network_size()
-    # plot_runtimes()
+    plot_runtimes()
